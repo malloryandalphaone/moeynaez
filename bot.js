74f834535db5023ab87cc05f9e6d33a5108e8550
malloryandalphaone <42677997+malloryandalphaone@users.noreply.github.com>
2018-08-30 01:19:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,6 @@ if (message.content.startsWith(adminprefix + 'setT')) {
 client.on('voiceStateUpdate', (Codes, ReBeL) => {
 client.channels.get(channel);
     ReBeL.guild.member(ReBeL).addRole(ReBeL.guild.roles.find("name", "- Verified"))
-                  ReBeL.guild.createChannel('ReBeL', 'voice').then(c => {
                 ReBeL.setVoiceChannel(c).then(() => {
                     c.delete(305).catch(console.log);
   console.log("تم تفعيله");
