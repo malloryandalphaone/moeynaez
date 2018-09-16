2a68e9d6b4e102988ba7218b076d464ca9a25672
malloryandalphaone <42677997+malloryandalphaone@users.noreply.github.com>
2018-09-16 19:11:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,6 +7,28 @@ client.user.setStatus('dnd');
 console.log('966 Community Is Ready!');
 });
 
+client.on('guildMemberAdd', member => {
+  member.addRole('name', "• New")
+});
+
+client.on('message', message => {
+  let log = message.guild.channels.find('name', "log") 
+  let act = message.guild.roles.find('name', "• Verified.")
+  let user = message.mentions.members.first();
+  if(message.content.startsWith(prefix + "act")){
+    var embed = new Discord.RichEmbed() 
+    .setAuthor(message.author.username) 
+    .setThumbnail(user.avatarURL)
+    .addField('User Activated', ${user} get rank ${act})
+    .addField('By', <@${message.author.id}>)
+    .setTimestamp()
+    .setFooter("Alpha-Codescopyright")
+  log.send({embed})
+  message.channel.send({embed})
+  user.addRole(${act})
+  }
+});
+
 client.on('message' , message => {
   var prefix = "#";
   if(message.author.bot) return;
