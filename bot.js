e076fc31fb4300f339fc7f4182f25baae9e39bcf
malloryandalphaone <42677997+malloryandalphaone@users.noreply.github.com>
2018-10-13 14:57:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -545,4 +545,28 @@ message.channel.send(`- ${user} Has ${inviteCount} invites.`);
   }
 });
 
+    const adminprefix = "$";
+const devs = ['380307890235506698','449506099268419595'];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'setgame')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+} else 
+  if (message.content.startsWith(adminprefix + 'setname')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`);
+} else
+  if (message.content.startsWith(adminprefix + 'setavatar')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else     
+if (message.content.startsWith(adminprefix + 'setT')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+}
+}); 
+
 client.login(process.env.BOT_TOKEN);
