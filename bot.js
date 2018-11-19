e26fc375ff73f38b5d784db204c3e3fcbc4cd98c
malloryandalphaone <42677997+malloryandalphaone@users.noreply.github.com>
2018-11-19 17:16:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -142,7 +142,7 @@ if(!check.voiceChannelID){
   hybhs++;
 }
 }
-guild.channels.find('id', '499201432667291668').setName("Voice "+hybhs+" ❃");
+guild.channels.find('id', '514076588892094464').setName("Voice "+hybhs+" ❃");
   client.setInterval(() =>{
     let d = Date.now()
   }, 5000);
@@ -153,10 +153,10 @@ let newUserChannel = newMember.voiceChannel
 let oldUserChannel = oldMember.voiceChannel
  if(oldUserChannel === undefined && newUserChannel !== undefined) {
    hybhs++;
-guild.channels.find('id', '499201432667291668').setName("Voice "+hybhs+" ❃");
+guild.channels.find('id', '514076588892094464').setName("Voice "+hybhs+" ❃");
 } else if(newUserChannel === undefined){
   hybhs--;
-guild.channels.find('id', '499201432667291668').setName("Voice "+hybhs+" ❃");
+guild.channels.find('id', '514076588892094464').setName("Voice "+hybhs+" ❃");
 }
 });
 
