f06324eebd75d2ca49889133bc96cbc924b13826
malloryandalphaone <42677997+malloryandalphaone@users.noreply.github.com>
2018-12-01 02:16:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const moment = require('moment');
 const fs = require('fs');
+const ms = require("ms");
 const client = new Discord.Client();
 const prefix = "$";
 client.on('ready', () => {
