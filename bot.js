f3cbf6ad11cbe1e2b67ba16f283db7755d6b953b
malloryandalphaone <42677997+malloryandalphaone@users.noreply.github.com>
2018-12-03 14:26:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -85,34 +85,6 @@ client.on("ready", () => {
 });
 
 
-
-client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.get("515563356006907923");
-    if (!channel) {
-        console.log("!the channel id it's not correct");
-        return;
-    }
-    if (member.id == client.user.id) {
-        return;
-    }
-    console.log('-');
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("488259622730203137");
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
- channel.send(`**Member Invite By:** ${Invite.inviter}`) ;         
- }
-            dat[Inv] = Invite.uses;
-       
-       });
-    });
-});
-
-
 let hybhs;
 client.on("ready", async  => {
     let guild = client.guilds.get("488259622730203137");
