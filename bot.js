cfe8c66e6a3351ecf98319fe97e89030e8aa2522
malloryandalphaone <42677997+malloryandalphaone@users.noreply.github.com>
2018-12-06 20:17:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,10 +5,10 @@ const moment = require('moment');
 const ytdl = require("ytdl-core");
 
 const prefix = "!";
+
 client.on("ready", () => {
-client.user.setStatus('idle');
   console.log("Reeebel | Logged in! Server count: ${client.guilds.size}");
-  client.user.setGame(`Strict.. !help`);
+  client.user.setGame(`Strict Clan | ${prefix}buy`);
 });
 
 client.on("message",async(message) => {
@@ -16,30 +16,29 @@ client.on("message",async(message) => {
 
   if (message.content.toLowerCase().startsWith(prefix + `help`)) {
     const embed = new Discord.RichEmbed()
-    .setTitle(`:mailbox_with_mail: Help Clan Strict..`)
+    .setTitle(`Help Command Strict Clan`)
     .setColor(`#FFFFFF`)
-    .setDescription(`هذا البوت للمساعدة فقط `)
-    .addField(`!تذكرة`, `عند مواجهتك لمشكلة بالفريق قم بكتابة هذا الأمر`)
-    .addField(`رابط`, `لأخذ رابط لجلب بعض الأعضاء`)
-    .addField(`!nick`, `لتغيير أسمك بالسيرفر`)
+    .addField(`Tickets`, ``)
+    .addField(`Invite`, ``)
+    .addField(`Other`, ``)
     message.channel.send({ embed: embed });
   }
 
 	
   if (message.content.toLowerCase().startsWith(prefix + `ping`)) {
     message.channel.send(`Hoold on!`).then(m => {
-    m.edit(`:ping_pong: Wew, made it over the ~waves~ ! **Pong!**\nMessage edit time is ` + (m.createdTimestamp - message.createdTimestamp) + `ms, Discord API heartbeat is ` + Math.round(client.ping) + `ms.`);
+    m.edit(`:ping_pong: \nMessage edit time is ` + (m.createdTimestamp - message.createdTimestamp) + `ms, Discord Strict.. ` + Math.round(client.ping) + `ms.`);
     });
 }
 
 
-    if (message.content.toLowerCase().startsWith(prefix + `clan`)) {
+    if (message.content.toLowerCase().startsWith(prefix + `buy`)) {
     const reason = message.content.split(" ").slice(1).join(" ");
-    if (!message.guild.roles.exists("name", "‹ Clan Support ›")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
+    if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
     if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
     await num++;
     await message.guild.createChannel(`ticket-${num}`, "text").then(c => {
-        let role = message.guild.roles.find("name", "‹ Clan Support ›");
+        let role = message.guild.roles.find("name", "Support Team");
         let role2 = message.guild.roles.find("name", "@everyone");
         c.overwritePermissions(role, {
             SEND_MESSAGES: true,
@@ -56,12 +55,12 @@ client.on("message",async(message) => {
         message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
         const embed = new Discord.RichEmbed()
         .setColor(0xCF40FA)
-        .addField(`Hey ${message.author.username}!`, `**أخوي الغالي، يرجى شرح سبب فتح التذكرة او المشكلة الي انت فيها واخوانا المساعدين ماراح يقصرون معك.**`)
+        .addField(`Hey ${message.author.username}!`, `**يرجى محاولة شرح سبب فتح هذه التذكرة بأكبر قدر ممكن من التفاصيل. سيكون فريق الدعم لدينا قريبا للمساعدة**`)
         .setTimestamp();
         c.send({ embed: embed });
     }).catch(console.error);
 }
-if (message.content.toLowerCase().startsWith(prefix + `قفل`)) {
+if (message.content.toLowerCase().startsWith(prefix + `c`)) {
     if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
 
     message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`-confirm\`. This will time out in 10 seconds and be cancelled.`)
