136d0076a8d8935fe0ec487eb6531c757ff3a956
malloryandalphaone <42677997+malloryandalphaone@users.noreply.github.com>
2019-01-01 03:52:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -654,6 +654,34 @@ client.on('message',async message => {
 };
  
 });
+
+
+  client.on("message", message => {
+ if(!message.channel.guild) return;  
+  if (message.author.bot) return;
+ 
+  let command = message.content.split(" ")[0];
+ 
+  if (message.content.split(" ")[0].toLowerCase() === prefix + "joined") {
+        if (!message.member.hasPermission('MANAGE_ROLES')) return;
+  let user = message.mentions.users.first();
+  let modlog = client.channels.find('name', 'log');
+  let muteRole = client.guilds.get(message.guild.id).roles.find('name', '-)»\' Mods.');
+  if (!muteRole) return message.reply("**I Can't Find rank ..**").catch(console.error).then(message => message.delete(4000))
+  if (message.mentions.users.size < 1) return message.reply('**Mention Any Member ..**').catch(console.error).then(message => message.delete(4000))
+  if (!message.guild.member(client.user).hasPermission('MANAGE_ROLES_OR_PERMISSIONS')) return;
+ 
+  if (message.guild.member(user).addRole(muteRole.id)) {
+      return message.channel.send(":white_check_mark: **User has Joined for Officials.**").catch(console.error).then(message => message.delete(4000))
+  } else {
+    message.guild.member(user).removeRole(muteRole).then(() => {
+      return message.channel.send(":white_check_mark: **User has Joined for Officials.**").catch(console.error).then(message => message.delete(4000))
+    });
+  }
+ 
+};
+ 
+});
  
 
 
