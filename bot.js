f562330e3493e29e596da61cf45662006c033718
malloryandalphaone <42677997+malloryandalphaone@users.noreply.github.com>
2019-01-02 06:24:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -229,10 +229,11 @@ client.on("message", message => {
 
 client.on("message", message => {
 
-            if (message.content.startsWith(prefix + "bc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  if (message.content.startsWith(prefix + "bc")) {
+  if (!message.member.hasPermission("ADMINISTRATOR"))  return;
   let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
+  var argresult = args.join(' ');
+ message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
  m.send(`${argresult}\n ${m}`);
 })
  message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
