3c21bb8f2dd9f832e5c14f27fed16943215cbaf8
malloryandalphaone <42677997+malloryandalphaone@users.noreply.github.com>
2019-01-07 12:29:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ var prefix = "!"
 const fs = require("fs"); 
 const moment = require("moment");  
 const ms = require("ms");
+const wait = require('util').promisify(setTimeout);
 
 client.on("ready", () => {
 client.user.setStatus('dnd');
@@ -14,8 +15,6 @@ client.user.setStatus('dnd');
 
 
 const invites = {};
-
-const wait = require('util').promisify(setTimeout);
 
 client.on('ready', () => {
   wait(1000);
@@ -143,6 +142,68 @@ client.on('message', message => {
     message.channel.send(':white_check_mark: **Done .**').then(msg => {msg.delete(10000)});
     }
     });
+
+
+
+
+
+let antibots = JSON.parse(fs.readFileSync('./antibots.json' , 'utf8'));//require antihack.json file
+client.on('message', message => {
+    if(message.content.startsWith(prefix + "antibots on")) {
+        if(!message.channel.guild) return message.reply('**This Command Only For Servers**');
+        if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('**Sorry But You Dont Have Permission** `MANAGE_GUILD`' );
+antibots[message.guild.id] = {
+onoff: 'On',
+}
+message.channel.send(`**✅ The AntiBots Is __𝐎𝐍__ !**`)
+          fs.writeFile("./antibots.json", JSON.stringify(antibots), (err) => {
+            if (err) console.error(err)
+            .catch(err => {
+              console.error(err);
+          });
+            });
+          }
+ 
+        })
+        //antibots with ON , OFF ! RARE CODE
+        //LIKE PLUSBOT !
+ 
+ 
+client.on('message', message => {
+    if(message.content.startsWith(prefix + "antibots off")) {
+        if(!message.channel.guild) return message.reply('**This Command Only For Servers**');
+        if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('**Sorry But You Dont Have Permission** `MANAGE_GUILD`' );
+antibots[message.guild.id] = {
+onoff: 'Off',
+}
+message.channel.send(`**⛔ The AntiBots Is __𝐎𝐅𝐅__ !**`)
+          fs.writeFile("./antibots.json", JSON.stringify(antibots), (err) => {
+            if (err) console.error(err)
+            .catch(err => {
+              console.error(err);
+          });
+            });
+          }
+ 
+        })
+ 
+client.on("guildMemberAdd", member => {
+  if(!antibots[member.guild.id]) antibots[member.guild.id] = {
+onoff: 'Off'
+}
+  if(antibots[member.guild.id].onoff === 'Off') return;
+if(member.user.bot) return member.kick()
+})
+ 
+fs.writeFile("./antibots.json", JSON.stringify(antibots), (err) => {
+if (err) console.error(err)
+.catch(err => {
+console.error(err);
+});
+ 
+})
+
+
 
 
 
