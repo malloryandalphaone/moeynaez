414673bd22b2b0ecbea09659db258149bf1fbeb3
malloryandalphaone <42677997+malloryandalphaone@users.noreply.github.com>
2019-01-14 17:15:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,7 +33,7 @@ client.on('guildMemberAdd', member => {
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
     const logChannel = member.guild.channels.find(channel => channel.name === "vast");
-    logChannel.send(`${member} Invited by: <@${inviter.id}>`);
+    logChannel.send(`${member} Invited by <@${inviter.id}>`);
   });
 });
 
