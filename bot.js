c0e0553b1faeb9c635196d9832ad93a679d049af
malloryandalphaone <42677997+malloryandalphaone@users.noreply.github.com>
2019-02-21 22:58:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,8 +60,8 @@ client.on('message', message => {
     if (message.author.bot) return;
 if (message.content.startsWith(prefix + 'clear')) { //Codes
     if(!message.channel.guild) return message.reply('This Command For Servers Only!'); 
-        if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('- **You dont have MANAGE_MESSAGES Permission.**');
-        if(!message.guild.member(client.user).hasPermission('MANAGE_MESSAGES')) return message.channel.send('- **I dont have MANAGE_MESSAGES Permission.**');
+        if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('- **You dont have Permission.**');
+        if(!message.guild.member(client.user).hasPermission('MANAGE_MESSAGES')) return message.channel.send('- **I dont have Permission.**');
  let args = message.content.split(" ").slice(1)
     let messagecount = parseInt(args);
     if (args > 99) return message.reply("- **يجب ان يكون عدد المسح أقل من 100 .**").then(messages => messages.delete(5000))
